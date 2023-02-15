8e236d1030514b307c22f4494208aa8018f21940
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-02-15 19:37:19 +0100

    feat(components): create icon component

---
+++ src/lib/components/icon/Icon.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let icon: string
+  export let iconDescription = ""
+</script>
+
+<!-- empty alt 'cause it's a decorative icon -->
+<img src={icon} alt={iconDescription} />
