5b82bcc0c5f89b62703dd9d657411241226fa205
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-02-15 19:36:59 +0100

    feat(components): create filled button using material3 specs

---
+++ src/lib/components/button/FilledButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let disabled = false
+  export let icon = null
+</script>
+
+<button on:click class="primary" class:disabled>
+  <slot />
+</button>
+
+<style lang="scss">
+  button {
+    border: 0;
+    cursor: pointer;
+    border-radius: 20px;
+    padding: 0 24px;
+    height: 40px;
+    font-weight: bold;
+    box-shadow: none;
+  }
+  .primary {
+    background: #6750a4;
+    color: white;
+  }
+  .primary.disabled {
+    pointer-events: none;
+    background-color: whitesmoke;
+    color: lightgrey;
+  }
+</style>
