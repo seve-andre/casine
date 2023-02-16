1cf5e5e998fd7b8cd0ca87e637569fda69a666f1
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-02-16 23:13:21 +0100

    feat(ui-components): add text button with material3 colors

---
+++ src/lib/ui-components/button/TextButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let disabled = false
+  // export let icon = null
+</script>
+
+<button on:click class="text-btn" class:disabled>
+  <slot />
+</button>
+
+<style>
+  .text-btn {
+    background-color: transparent;
+    color: #6750a4;
+  }
+
+  .text-btn.disabled {
+    pointer-events: none;
+
+    /* 1f = 12% opacity */
+    /* background-color: #1c1b1f1f; */
+
+    /* 61 = 38% opacity */
+    /* color: #1c1b1f61; */
+  }
+
+  .text-btn:hover {
+    /* box-shadow: 1px 0px 1px black; */
+  }
+
+  .text-btn:focus {
+  }
+</style>
