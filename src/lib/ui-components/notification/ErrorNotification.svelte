d292a51c4151b32168e83ccd0de5b053d5548aab
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-02-22 22:00:44 +0100

    feat(ui-components): create notifications

---
+++ src/lib/ui-components/notification/ErrorNotification.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import BaseNotification from "./BaseNotification.svelte"
+  import { Cross2 } from "radix-icons-svelte"
+
+  export let title = ""
+</script>
+
+<BaseNotification {title} icon={Cross2} color="red">
+  <slot />
+</BaseNotification>
