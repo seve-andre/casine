f098ee979f4bf0b0344f0b6f24a607669d68435e
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-03-13 12:27:05 +0100

    add minimal homepage using flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,83 @@
 <script lang="ts">
-  import { Button, Tooltip } from "flowbite-svelte"
+  import { Heading, Tooltip } from "flowbite-svelte"
   import { goto } from "$app/navigation"
   import FilledButton from "@/lib/ui-components/button/FilledButton.svelte"
+  import type { Apartment } from "@/models/Apartment"
+  import { gotoApartmentInHouse, gotoBookings, gotoPrices } from "./navigation"
+
+  let apartmentsHouseA: Apartment[] = [
+    {
+      house_id: 1,
+      apartment_number: 1,
+    },
+    {
+      house_id: 1,
+      apartment_number: 2,
+    },
+    {
+      house_id: 1,
+      apartment_number: 3,
+    },
+    {
+      house_id: 1,
+      apartment_number: 4,
+    },
+    {
+      house_id: 1,
+      apartment_number: 5,
+    },
+    {
+      house_id: 1,
+      apartment_number: 6,
+    },
+  ]
 </script>
 
-<!-- on:click={() => gotoApartmentInHouse(1, "A")} -->
+<div class="home-container">
+  <div class="apartment-selection">
+    <Heading customSize="text-2xl font-bold">Casa A</Heading>
+    <div class="grid grid-cols-3 gap-4">
+      {#each apartmentsHouseA as apartment}
+        <FilledButton on:click={() => gotoApartmentInHouse(apartment.apartment_number, "A")}>
+          App. {apartment.apartment_number}
+        </FilledButton>
+        <Tooltip placement="bottom">Vai all'appartamento {apartment.apartment_number}</Tooltip>
+      {/each}
+    </div>
+  </div>
+  <div class="bookings-utils">
+    <Heading tag="h2" customSize="text-xl font-semibold">Utili</Heading>
+    <FilledButton on:click={() => gotoBookings()}>Prenotazioni</FilledButton>
+    <Tooltip placement="bottom">Vedi prenotazioni</Tooltip>
 
-<!-- <Button on:click={() => goto("/house/A/apartment/1")} pill>App. 1</Button> -->
+    <FilledButton on:click={() => gotoPrices()}>Prezzi</FilledButton>
+    <Tooltip placement="bottom">Vedi prezzi</Tooltip>
+  </div>
+  <div class="form" />
+</div>
 
-<FilledButton on:click={() => goto("/house/A/apartment/1")}>App. 1</FilledButton>
-<Tooltip placement="bottom">Vai all'appartmento 1</Tooltip>
+<style>
+  .home-container {
+    display: grid;
+    grid-template-columns: 1fr 1.5fr;
+    grid-template-rows: 1.5fr 1fr;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+      "apartment-selection form"
+      "bookings-utils .";
+    height: 100%;
+  }
+
+  .apartment-selection {
+    grid-area: apartment-selection;
+  }
+
+  .bookings-utils {
+    grid-area: bookings-utils;
+  }
+
+  .form {
+    grid-area: form;
+  }
+</style>
