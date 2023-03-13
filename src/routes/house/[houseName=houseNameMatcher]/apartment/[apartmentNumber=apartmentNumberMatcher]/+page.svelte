501f844e0f444f7ba5a7bcc32ff75d1393f7c643
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-03-13 19:40:41 +0100

    replace svg with corresponding ui component icon

--- src/routes/house/[houseName=houseNameMatcher]/apartment/[apartmentNumber=apartmentNumberMatcher]/+page.svelte
+++ src/routes/house/[houseName=houseNameMatcher]/apartment/[apartmentNumber=apartmentNumberMatcher]/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { page } from "$app/stores"
-  import FilledButton from "@/lib/ui-components/button/FilledButton.svelte"
-
+  import LeftArrow from "@/lib/ui-components/icon/LeftArrow.svelte"
+  import RightArrow from "@/lib/ui-components/icon/RightArrow.svelte"
   import {
     Button,
-    Search,
     Table,
     TableBody,
     TableBodyCell,
@@ -12,22 +11,11 @@
     TableHead,
     TableHeadCell,
     StepIndicator,
-    Datepicker,
     Heading,
     PaginationItem,
-    P,
   } from "flowbite-svelte"
 
-  let searchTerm = ""
-  let items = [
-    { id: 1, maker: "Toyota", type: "ABC", make: 2017 },
-    { id: 2, maker: "Ford", type: "CDE", make: 2018 },
-    { id: 3, maker: "Volvo", type: "FGH", make: 2019 },
-    { id: 4, maker: "Saab", type: "IJK", make: 2020 },
-  ]
-  $: filteredItems = items.filter(item => item.maker.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)
-
-  let guests = []
+  let guests = [1]
 
   // guests == 0 data
   let currentStep = 1
@@ -36,20 +24,8 @@
   const nextStep = () => (currentStep += 1)
   const openApartment = () => {}
 
-  const stepsTest = {
-    1: {
-      title: "1 - Scegli periodo",
-      content: "",
-      nextLabel: "Avanti",
-      nextAction: nextStep,
-    },
-    2: {
-      title: "2 - Aggiungi capogruppo",
-      content: "",
-      nextLabel: "Fine",
-      nextAction: openApartment,
-    },
-  }
+  $: nextLabel = currentStep == 1 ? "Avanti" : "Fine"
+  $: nextAction = currentStep == 1 ? nextStep : openApartment
 
   // guests > 0 data
   let tableHeads = ["Nome", "Cognome", "Data di nascita"]
@@ -62,21 +38,27 @@
   ]
 </script>
 
-<div class="h-screen flex flex-col">
+<div class="h-screen flex flex-col gap-4">
   <div class="flex-initial">
-    <Heading customSize="text-3xl font-bold">Casa A - Appartamento {$page.params.apartmentNumber}</Heading>
+    <Heading customSize="text-3xl font-bold">
+      Casa {$page.params.houseName} - Appartamento {$page.params.apartmentNumber}
+    </Heading>
   </div>
 
   <div class="flex-auto">
     {#if guests.length == 0}
-      <StepIndicator {currentStep} {steps} />
+      <div class="flex flex-col gap-2">
+        <StepIndicator {currentStep} {steps} />
 
-      <!-- content belonging to step -->
-      {#if currentStep == 1}
-        <Datepicker datepickerTitle="Periodo di permanenza" range datepickerFormat="dd/mm/yyyy" />
-      {:else}
-        <Button>2</Button>
-      {/if}
+        <!-- content belonging to step -->
+        <div>
+          {#if currentStep == 1}
+            <!-- <Datepicker on:click={() => console.log("ifewofwo")} /> -->
+          {:else}
+            <Button>2</Button>
+          {/if}
+        </div>
+      </div>
     {:else}
       <div>
         <Table striped>
@@ -103,29 +85,13 @@
     <div class="flex space-x-3">
       {#if currentStep != 1}
         <PaginationItem class="flex items-center" on:click={prevStep}>
-          <svg class="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path
-              fill-rule="evenodd"
-              d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <LeftArrow clazz="mr-2 w-5 h-5" />
           Torna a scelta periodo
         </PaginationItem>
       {/if}
-      <PaginationItem class="flex items-center" on:click={currentStep == 1 ? nextStep : openApartment}>
-        {#if currentStep == 1}
-          Avanti
-        {:else}
-          Fine
-        {/if}
-        <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fill-rule="evenodd"
-            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          />
-        </svg>
+      <PaginationItem class="flex items-center" on:click={nextAction}>
+        {nextLabel}
+        <RightArrow clazz="ml-2 w-5 h-5" />
       </PaginationItem>
     </div>
   </div>
