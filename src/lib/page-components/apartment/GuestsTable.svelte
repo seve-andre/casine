a963a0cfd55c28b8df054807d8b73a99bfa4af3e
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-03-20 21:46:18 +0100

    add guest mapper for table from guests pulled from db

--- src/lib/page-components/apartment/GuestsTable.svelte
+++ src/lib/page-components/apartment/GuestsTable.svelte
@@ -1,30 +1,48 @@
 <script lang="ts">
+  import { calculate_age } from "@/lib/utils/DateUtils"
   import type { Guest } from "@/models/Guest"
   import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from "flowbite-svelte"
 
-  let tableHeaders = ["Nome", "Cognome", "Data di nascita"]
+  let guestsInfo = ["Nome", "Cognome", "Data di nascita", "Età"]
+
+  // guests need to be mapped with only the values needed by the table
   let guests: Guest[] = [
     {
       id: 1,
       first_name: "Andrea",
       last_name: "Severi",
-      birth_date: "02-10-2000",
+      birth_date: "2000-10-02",
+    },
+    {
+      id: 2,
+      first_name: "Chiara",
+      last_name: "Leonelli",
+      birth_date: "2002-07-30",
     },
   ]
+
+  let guestsMapped = guests.map(g => ({
+    id: g.id,
+    firstName: g.first_name,
+    lastName: g.last_name,
+    birthDate: g.birth_date,
+    age: calculate_age(new Date(g.birth_date)),
+  }))
 </script>
 
 <Table striped>
   <TableHead>
-    {#each tableHeaders as header}
-      <TableHeadCell>{header}</TableHeadCell>
+    {#each guestsInfo as info}
+      <TableHeadCell>{info}</TableHeadCell>
     {/each}
   </TableHead>
   <TableBody>
-    {#each guests as guest (guest.id)}
+    {#each guestsMapped as guest (guest.id)}
       <TableBodyRow>
-        <TableBodyCell>{guest.first_name}</TableBodyCell>
-        <TableBodyCell>{guest.last_name}</TableBodyCell>
-        <TableBodyCell>{guest.birth_date}</TableBodyCell>
+        <TableBodyCell>{guest.firstName}</TableBodyCell>
+        <TableBodyCell>{guest.lastName}</TableBodyCell>
+        <TableBodyCell>{guest.birthDate}</TableBodyCell>
+        <TableBodyCell>{guest.age}</TableBodyCell>
       </TableBodyRow>
     {/each}
   </TableBody>
