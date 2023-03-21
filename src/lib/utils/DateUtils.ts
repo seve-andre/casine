1590aee3223472a1049ce80691c8a871ba2f2216
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-03-21 22:07:08 +0100

    add return type to calculate_age util function

--- src/lib/utils/DateUtils.ts
+++ src/lib/utils/DateUtils.ts
@@ -1,4 +1,4 @@
-export function calculate_age(dob: Date) {
+export function calculate_age(dob: Date): number {
     let diff_ms = Date.now() - dob.getTime()
     let age_dt = new Date(diff_ms)
     return Math.abs(age_dt.getUTCFullYear() - 1970)
