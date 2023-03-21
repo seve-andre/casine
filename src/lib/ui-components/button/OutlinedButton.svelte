e0a8b35e02031315e3c2a5bea4fccfb1c8ed9326
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-03-21 22:25:27 +0100

    add colors to outlined button

--- src/lib/ui-components/button/OutlinedButton.svelte
+++ src/lib/ui-components/button/OutlinedButton.svelte
@@ -2,8 +2,9 @@
   import { Button } from "flowbite-svelte"
 
   export let disabled = false
+  export let color: "blue" | "red" | "dark" = "blue"
 </script>
 
-<Button on:click pill outline {disabled}>
+<Button on:click pill outline {disabled} {color}>
   <slot />
 </Button>
