9a978ef017f585085572fb3aca438b12f1382d58
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-13 16:00:39 +0200

    add queryable Rent struct

--- src-tauri/src/models/rent.rs
+++ src-tauri/src/models/rent.rs
@@ -1,7 +1,20 @@
 use crate::schema::rents;
 use chrono::NaiveDate;
-use diesel::Insertable;
-use serde::Serialize;
+use diesel::{Associations, Identifiable, Insertable, Queryable};
+use serde::{Deserialize, Serialize};
+
+use super::apartment::Apartment;
+use super::group::Group;
+
+#[derive(Identifiable, Queryable, Associations, Serialize, Deserialize)]
+#[diesel(table_name = rents, belongs_to(Group), belongs_to(Apartment))]
+pub struct Rent {
+    pub id: i32,
+    pub start_date: NaiveDate,
+    pub end_date: NaiveDate,
+    pub group_id: i32,
+    pub apartment_id: i32,
+}
 
 #[derive(Insertable, Serialize)]
 #[diesel(table_name = rents)]
