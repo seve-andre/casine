5fc88f9fee28059385d96ab874e682da43971fd1
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-13 16:01:17 +0200

    add query to get guests for apartment

--- src-tauri/src/commands/db_commands.rs
+++ src-tauri/src/commands/db_commands.rs
@@ -1,6 +1,12 @@
 // see https://tauri.app/v1/guides/features/command
-use crate::{db::establish_connection, errors::MyError, models::apartment::Apartment, schema};
-use diesel::prelude::*;
+use crate::{
+    db::establish_connection,
+    errors::MyError,
+    models::{apartment::Apartment, guest::Guest},
+    schema::{self, apartments::apartment_number, rents},
+};
+use chrono::{NaiveDate, NaiveTime, Utc};
+use diesel::{prelude::*, sql_types::Bool};
 
 /*
     SELECT *
@@ -17,6 +23,49 @@ pub async fn get_apartments() -> Result<Vec<Apartment>, MyError> {
         .map_err(MyError::DatabaseQueryError);
 }
 
-// see at https://docs.diesel.rs/2.0.x/diesel/associations/index.html
-// #[tauri::command]
-// pub async fn get_apartments_by_house() -> Result<Vec<(House, Vec<Apartment>)>, MyError>
+pub async fn get_apartment_by_house_name_and_number(
+    p_house_name: String,
+    p_apartment_number: i32,
+) -> Result<i32, MyError> {
+    use schema::apartments::dsl::*;
+
+    let connection = &mut establish_connection()?;
+
+    return apartments
+        .filter(apartment_number.eq(p_apartment_number))
+        .filter(house_name.eq(p_house_name))
+        .select(id)
+        .get_result::<i32>(connection)
+        .map_err(MyError::DatabaseQueryError);
+}
+
+/*
+    SELECT *
+    FROM guests
+*/
+pub async fn get_guests(
+    p_house_name: String,
+    p_apartment_number: i32,
+) -> Result<Vec<Guest>, MyError> {
+    use schema::rents::dsl::*;
+
+    let connection = &mut establish_connection()?;
+
+    // get apartment_id from house name and number
+    let apartment_id_result =
+        get_apartment_by_house_name_and_number(p_house_name, p_apartment_number).await?;
+
+    // based on date and apartment_id, get group_id
+    let today_date = Utc::now().date_naive();
+
+    let group_id_result = rents
+        .filter(apartment_id.eq(apartment_id_result))
+        .filter(start_date.le(&today_date))
+        .filter(end_date.ge(&today_date))
+        .select(group_id)
+        .get_result::<i32>(connection);
+
+    // from group_id, get guest_id -> Guest
+
+    return Result::Ok(vec![]);
+}
