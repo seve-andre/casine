c57d63cd7ea6cc2c61df6e770e233ef763db36a5
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-17 20:20:00 +0200

    add coercion for dates

--- src/models/Guest.ts
+++ src/models/Guest.ts
@@ -4,5 +4,5 @@ export const Guest = z.object({
     id: z.number(),
     first_name: z.string(),
     last_name: z.string(),
-    birth_date: z.date()
+    birth_date: z.coerce.date()
 })
--- src/models/Rent.ts
+++ src/models/Rent.ts
@@ -4,6 +4,6 @@ export const Rent = z.object({
     id: z.number(),
     apartment_id: z.number().lte(12),
     group_id: z.number(),
-    start_date: z.date(),
-    end_date: z.date(),
+    start_date: z.coerce.date(),
+    end_date: z.coerce.date(),
 })
