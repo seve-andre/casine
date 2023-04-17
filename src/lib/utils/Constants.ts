7f0a9aab2e9021ea4d6639d48f4c340cb2cbb757
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-17 22:27:12 +0200

    add constants for apartment ids

--- src/lib/utils/Constants.ts
+++ src/lib/utils/Constants.ts
@@ -1,10 +1,14 @@
-
+// these are harcoded since there's a finite number of apartments
 const minApartmentNumber = 1
 const maxApartmentNumber = 6
+const minApartmentId = 1
+const maxApartmentId = 12
 
 export const Constants = {
     houseRegex: /A|B/,
     apartmentRegex: new RegExp(`[${minApartmentNumber}-${maxApartmentNumber}]`),
     minApartmentNumber: minApartmentNumber,
-    maxApartmentNumber: maxApartmentNumber
+    maxApartmentNumber: maxApartmentNumber,
+    minApartmentId: minApartmentId,
+    maxApartmentId: maxApartmentId
 } as const
--- src/models/Rent.ts
+++ src/models/Rent.ts
@@ -1,8 +1,9 @@
 import z from "zod"
+import { Constants } from "~/lib/utils/Constants"
 
 export const Rent = z.object({
     id: z.number(),
-    apartment_id: z.number().lte(12),
+    apartment_id: z.number().min(Constants.minApartmentId).max(Constants.maxApartmentId),
     group_id: z.number(),
     start_date: z.coerce.date(),
     end_date: z.coerce.date(),
