ea256db57d51453f62b6d4b5898f6e2cdceaf5e7
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-17 23:23:09 +0200

    remove old matchers

--- src/params/houseNameMatcher.ts
+++
@@ -1,6 +0,0 @@
-import type { ParamMatcher } from "@sveltejs/kit"
-import { Constants } from "~/lib/utils/Constants"
-
-export const match = ((param) => {
-  return Constants.houseRegex.test(param)
-}) satisfies ParamMatcher
