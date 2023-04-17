6c131bc5c58f984275fb63fac1e77a5cde096ab4
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-17 20:57:00 +0200

    add apartment page error handling and schema validation

--- src/routes/house/[houseName=houseNameMatcher]/apartment/[apartmentNumber=apartmentNumberMatcher]/+page.ts
+++ src/routes/house/[houseName=houseNameMatcher]/apartment/[apartmentNumber=apartmentNumberMatcher]/+page.ts
@@ -1,19 +1,32 @@
-import { invoke } from "@tauri-apps/api"
+import z from "zod"
 import type { PageLoad } from "./$types"
-import type { Guest } from "~/models/Guest"
-import type { Rent } from "~/models/Rent"
+import { error } from "@sveltejs/kit"
+import { invoke } from "@tauri-apps/api"
+import { Guest } from "~/models/Guest"
+import { Rent } from "~/models/Rent"
 
 export const load = (async ({ params }) => {
-    return {
-      title: `Casa ${params.houseName} - Appartamento ${params.apartmentNumber}`,
-      description: "Vedi, aggiungi o rimuovi gli ospiti dall'appartamento",
-      rentInfo: {
-        id: 1,
-        apartment_id: params.apartmentNumber,
-        group_id: 1,
-        start_date: new Date("2023-05-01"),
-        end_date: new Date("2023-05-30"),
-      } as unknown as Rent,
-      guests: await invoke<Guest[]>("get_guests", { pHouseName: params.houseName, pApartmentNumber: +params.apartmentNumber})
+  const guestsArrayParser = z.array(Guest)
+  const guestsResult = guestsArrayParser.safeParse(await invoke("get_guests", { pHouseName: params.houseName, pApartmentNumber: +params.apartmentNumber}))
+
+  const rentResult = Rent.safeParse(
+    {
+      id: 1,
+      apartment_id: +params.apartmentNumber,
+      group_id: 1,
+      start_date: "2023-05-01",
+      end_date: "2023-05-30",
     }
+  )
+
+  if (!guestsResult.success || !rentResult.success) {
+    throw error(404, "Not found")
+  }
+
+  return {
+    title: `Casa ${params.houseName} - Appartamento ${params.apartmentNumber}`,
+    description: "Vedi, aggiungi o rimuovi gli ospiti dall'appartamento",
+    rentInfo: rentResult.data,
+    guests: guestsResult.data
+  }
 }) satisfies PageLoad
