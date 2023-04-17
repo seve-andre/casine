b21dc815e61f06a2e73a44a58861b4002b2936a5
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-17 09:51:17 +0200

    add spinner on page loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import "~/app.postcss"
-  import { page } from "$app/stores"
-  import { Button, Navbar } from "flowbite-svelte"
+  import { navigating, page } from "$app/stores"
+  import { Spinner, Button, Navbar } from "flowbite-svelte"
   import LeftArrow from "~/lib/ui-components/icon/LeftArrow.svelte"
 </script>
 
@@ -9,17 +9,23 @@
   <title>{$page.data.title}</title>
   <meta name="description" content={$page.data.description} />
 </svelte:head>
-<div class="flex flex-col h-screen">
-  {#if $page.url.pathname != "/"}
-    <header class="flex-initial">
-      <Navbar class="bg-gray-50 dark:bg-gray-900">
-        <Button href="/" pill color="light" outline>
-          <LeftArrow clazz="w-4 h-4" />
-        </Button>
-      </Navbar>
-    </header>
-  {/if}
-  <main class="px-10 py-5 h-screen flex-auto overflow-hidden bg-gray-50 dark:bg-gray-900">
-    <slot />
-  </main>
-</div>
+{#if $navigating}
+  <div class="h-screen flex justify-center items-center">
+    <Spinner />
+  </div>
+{:else}
+  <div class="flex flex-col h-screen">
+    {#if $page.url.pathname != "/"}
+      <header class="flex-initial">
+        <Navbar class="bg-gray-50 dark:bg-gray-900">
+          <Button href="/" pill color="light" outline>
+            <LeftArrow clazz="w-4 h-4" />
+          </Button>
+        </Navbar>
+      </header>
+    {/if}
+    <main class="px-10 py-5 h-screen flex-auto overflow-hidden bg-gray-50 dark:bg-gray-900">
+      <slot />
+    </main>
+  </div>
+{/if}
