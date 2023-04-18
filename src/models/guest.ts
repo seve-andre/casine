cf2623508093c67ed02d14f66c258fbea45cd0c0
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-18 18:07:29 +0200

    add min length for first and last name in guest

--- src/models/guest.ts
+++ src/models/guest.ts
@@ -2,8 +2,8 @@ import z from "zod"
 
 export const GuestSchema = z.object({
     id: z.number(),
-    first_name: z.string(),
-    last_name: z.string(),
+    first_name: z.string().min(1),
+    last_name: z.string().min(1),
     birth_date: z.coerce.date()
 })
 export type Guest = z.infer<typeof GuestSchema>
