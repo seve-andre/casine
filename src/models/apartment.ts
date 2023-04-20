fbc42779e23bcad3f5b5c78d45fcf1a506b38e44
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-20 14:10:09 +0200

    add min/max constraint on apartment zod schema

--- src/models/apartment.ts
+++ src/models/apartment.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 import { Constants } from "~/lib/utils/constants"
 
 export const ApartmentSchema = z.object({
-    id: z.number(),
+    id: z.number().min(Constants.minApartmentId).max(Constants.maxApartmentId),
     house_name: z.string().regex(Constants.houseRegex),
     apartment_number: z.number().min(Constants.minApartmentNumber).max(Constants.maxApartmentNumber)
 })
