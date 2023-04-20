dbf545b540272d90abfee901bcf57d4a634ff373
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-20 14:12:05 +0200

    remove useless function and change command params

--- src-tauri/src/commands/db_commands.rs
+++ src-tauri/src/commands/db_commands.rs
@@ -23,21 +23,6 @@ pub async fn get_apartments() -> Result<Vec<Apartment>, MyError> {
         .map_err(MyError::DatabaseQueryError);
 }
 
-fn get_apartment_by_house_name_and_number(
-    p_house_name: String,
-    p_apartment_number: i32,
-) -> Result<Apartment, MyError> {
-    use schema::apartments::dsl::*;
-
-    let connection = &mut establish_connection()?;
-
-    return apartments
-        .filter(apartment_number.eq(p_apartment_number))
-        .filter(house_name.eq(p_house_name))
-        .get_result::<Apartment>(connection)
-        .map_err(MyError::DatabaseQueryError);
-}
-
 type DB = diesel::mysql::Mysql;
 fn is_date_in_range(
     date: NaiveDate,
@@ -75,12 +60,10 @@ fn get_guests_ids_by_group_id(group_id_result: i32) -> Result<Vec<i32>, MyError>
     TODO
 */
 #[tauri::command]
-pub async fn get_guests(house_name: String, apartment_number: i32) -> Result<Vec<Guest>, MyError> {
+pub async fn get_guests(apartment: Apartment) -> Result<Vec<Guest>, MyError> {
     use schema::guests::dsl::*;
 
     let connection = &mut establish_connection()?;
-
-    let apartment = get_apartment_by_house_name_and_number(house_name, apartment_number)?;
 
     let group_id_result = get_group_id_by_apartment(&apartment);
     let actual_group_id = match group_id_result {
