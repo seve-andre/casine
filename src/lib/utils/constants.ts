ad6b847df2c31aa87bbad42033a2ec6271453b9a
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-22 15:32:54 +0200

    Update constants.ts

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,14 +1,9 @@
-// these are harcoded since there's a finite number of apartments
-const minApartmentNumber = 1
-const maxApartmentNumber = 6
-const minApartmentId = 1
-const maxApartmentId = 12
-
 export const Constants = {
     houseRegex: /A|B/,
     apartmentRegex: /[1-9]|1[0-2]/,
-    minApartmentNumber: minApartmentNumber,
-    maxApartmentNumber: maxApartmentNumber,
-    minApartmentId: minApartmentId,
-    maxApartmentId: maxApartmentId
+    // these are harcoded since there's a finite number of apartments
+    minApartmentId: 1,
+    maxApartmentId: 12,
+    minApartmentNumber: 1,
+    maxApartmentNumber: 6
 } as const
