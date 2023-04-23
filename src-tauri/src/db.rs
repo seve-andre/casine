ee2e770e6572835a3e5f5708e8f9a9bb96f7745d
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-23 10:02:19 +0200

    remove comment

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -1,8 +1,9 @@
-use crate::errors::MyError;
 use diesel::prelude::*;
 use diesel::MysqlConnection;
 use dotenvy::dotenv;
 use std::env;
+
+use crate::errors::MyError;
 
 pub fn establish_connection() -> Result<MysqlConnection, MyError> {
     dotenv().ok();
@@ -10,5 +11,4 @@ pub fn establish_connection() -> Result<MysqlConnection, MyError> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     MysqlConnection::establish(&database_url).map_err(MyError::DatabaseConnectionError)
-    // .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
