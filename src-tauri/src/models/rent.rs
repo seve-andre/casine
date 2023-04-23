ca8ed3748da47be87a3048d8219280cc0fd6b82d
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-23 10:03:25 +0200

    split rent attributes to multiline

--- src-tauri/src/models/rent.rs
+++ src-tauri/src/models/rent.rs
@@ -1,13 +1,15 @@
 use crate::schema::rents;
 use chrono::NaiveDate;
-use diesel::{Associations, Identifiable, Insertable, Queryable};
+use diesel::{Associations, Identifiable, Insertable, Queryable, Selectable};
 use serde::{Deserialize, Serialize};
 
 use super::apartment::Apartment;
 use super::group::Group;
 
-#[derive(Identifiable, Queryable, Associations, Serialize, Deserialize, Debug)]
-#[diesel(table_name = rents, belongs_to(Group), belongs_to(Apartment))]
+#[derive(Identifiable, Selectable, Queryable, Associations, Serialize, Deserialize, Debug)]
+#[diesel(belongs_to(Group))]
+#[diesel(belongs_to(Apartment))]
+#[diesel(table_name = rents)]
 pub struct Rent {
     pub id: i32,
     pub apartment_id: i32,
