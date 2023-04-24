2698ad8ea729fb5497d70a3b2f94213e4198b132
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-24 12:22:03 +0200

    add `open_apartment` to commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -16,7 +16,8 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             db_commands::get_apartments,
             db_commands::get_guests_in_apartment,
-            db_commands::get_apartment_by_id
+            db_commands::get_apartment_by_id,
+            db_commands::open_apartment
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
