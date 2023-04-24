c3b5502b3feab61adc8a7215a0aa444c7960a837
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-24 12:22:22 +0200

    create zod schema for `NewGroup`

--- src/models/group.ts
+++ src/models/group.ts
@@ -4,5 +4,7 @@ export const GroupSchema = z.object({
     id: z.number().min(1),
     nickname: z.string().min(1)
 })
-
 export type Group = z.infer<typeof GroupSchema>
+
+export const NewGroupSchema = GroupSchema.omit({ id: true })
+export type NewGroup = z.infer<typeof NewGroupSchema>
