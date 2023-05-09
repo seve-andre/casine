5fc3a2296e9dc9284d61e07a9b111eb7a1371872
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-05-09 12:41:46 +0200

    from "form" to "sidebar" layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,90 @@
-<script>
+<script lang="ts">
   import "~/app.postcss"
   import { navigating, page } from "$app/stores"
-  import { Spinner, Button, Navbar } from "flowbite-svelte"
-  import LeftArrow from "~/lib/ui-components/icon/LeftArrow.svelte"
+  import { Spinner, Sidebar, SidebarWrapper, SidebarGroup, SidebarItem, Navbar, Button } from "flowbite-svelte"
+  import { HomeFilled, HomeOutlined, LeftArrow } from "~/lib/ui-components"
+
+  $: activeUrl = $page.url.pathname
+
+  const sidebarItems = [
+    {
+      name: "Home",
+      href: "/",
+      outlinedIcon: HomeOutlined,
+      filledIcon: HomeFilled,
+    },
+    {
+      name: "Bookings",
+      href: "/bookings",
+      outlinedIcon: LeftArrow,
+      filledIcon: HomeFilled,
+    },
+    {
+      name: "Prices",
+      href: "/prices",
+      outlinedIcon: LeftArrow,
+      filledIcon: HomeFilled,
+    },
+  ]
 </script>
 
+<!-- https://kit.svelte.dev/docs/seo#manual-setup-title-and-meta -->
 <svelte:head>
   <title>{$page.data.title}</title>
   <meta name="description" content={$page.data.description} />
 </svelte:head>
+
 {#if $navigating}
   <div class="h-screen flex justify-center items-center">
     <Spinner />
   </div>
 {:else}
-  <div class="flex flex-col h-screen">
-    {#if $page.url.pathname != "/"}
-      <header class="flex-initial">
-        <Navbar class="bg-gray-50 dark:bg-gray-900">
-          <Button href="/" pill color="light" outline>
-            <LeftArrow clazz="w-4 h-4" />
-          </Button>
-        </Navbar>
-      </header>
-    {/if}
-    <main class="px-10 py-5 h-screen flex-auto overflow-hidden bg-gray-50 dark:bg-gray-900">
-      <slot />
-    </main>
+  <div class="layout-container h-screen">
+    <div class="sidebar bg-gray-50">
+      <Sidebar>
+        <SidebarWrapper>
+          <SidebarGroup>
+            {#each sidebarItems as item}
+              {@const isActive = activeUrl === item.href}
+              <SidebarItem label={item.name} href={item.href} active={isActive}>
+                <svelte:fragment slot="icon">
+                  <svelte:component this={isActive ? item.filledIcon : item.outlinedIcon} class="w-6 h-6" />
+                </svelte:fragment>
+              </SidebarItem>
+            {/each}
+          </SidebarGroup>
+        </SidebarWrapper>
+      </Sidebar>
+    </div>
+
+    <div class="content-container">
+      {#if ["/", "/bookings", "/prices"].includes(activeUrl) === false}
+        <header>
+          <Navbar>
+            <Button href="/" pill color="light" outline>
+              <LeftArrow class="w-4 h-4" />
+            </Button>
+          </Navbar>
+        </header>
+      {/if}
+
+      <main>
+        <slot />
+      </main>
+    </div>
   </div>
 {/if}
+
+<style>
+  .layout-container {
+    display: flex;
+  }
+
+  .sidebar {
+    flex-basis: 20%;
+  }
+
+  .content-container {
+    flex-basis: 80%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,60 +2,18 @@
   import type { PageData } from "./$types"
   import { Heading, Tooltip } from "flowbite-svelte"
   import FilledButton from "~/lib/ui-components/button/FilledButton.svelte"
-  import NewGuestForm from "~/lib/page-components/common/NewGuestForm.svelte"
 
   export let data: PageData
 </script>
 
-<div class="home-container">
-  <div class="apartment-selection flex flex-col gap-4">
-    <Heading customSize="text-2xl font-bold">Casa A</Heading>
-    <div class="grid grid-cols-3 gap-4">
-      {#each data.apartments as apartment}
-        <FilledButton href={`apartments/${apartment.id}`}>
-          App. {apartment.apartment_number}
-        </FilledButton>
-        <Tooltip placement="bottom">Vai all'appartamento {apartment.apartment_number}</Tooltip>
-      {/each}
-    </div>
-  </div>
-  <div class="bookings-utils flex flex-col gap-4">
-    <Heading tag="h2" customSize="text-xl font-semibold">Utili</Heading>
-    <div class="grid grid-cols-3 gap-4">
-      <FilledButton href="/bookings">Prenotazioni</FilledButton>
-      <Tooltip placement="bottom">Vedi prenotazioni</Tooltip>
-
-      <FilledButton href="/prices">Prezzi</FilledButton>
-      <Tooltip placement="bottom">Vedi prezzi</Tooltip>
-    </div>
-  </div>
-  <div class="form">
-    <NewGuestForm apartments={data.apartments} />
+<div class="flex flex-col gap-4 p-10">
+  <Heading customSize="text-2xl font-bold">Casa A</Heading>
+  <div class="grid grid-cols-3 gap-10">
+    {#each data.apartments as apartment}
+      <FilledButton href={`apartments/${apartment.id}`}>
+        App. {apartment.apartment_number}
+      </FilledButton>
+      <Tooltip placement="bottom">Vai all'appartamento {apartment.apartment_number}</Tooltip>
+    {/each}
   </div>
 </div>
-
-<style>
-  .home-container {
-    display: grid;
-    grid-template-columns: 1fr 1.5fr;
-    grid-template-rows: 1.5fr 1fr;
-    grid-auto-flow: row;
-    grid-template-areas:
-      "apartment-selection form"
-      "bookings-utils form";
-    height: 100%;
-  }
-
-  .apartment-selection {
-    grid-area: apartment-selection;
-  }
-
-  .bookings-utils {
-    grid-area: bookings-utils;
-  }
-
-  .form {
-    grid-area: form;
-    padding-left: 5%;
-  }
-</style>
