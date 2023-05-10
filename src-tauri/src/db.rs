4c479bd92ec26ff64d95fd65e472c10f0c7c55fe
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-05-10 23:31:12 +0200

    remove `.env` file

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -1,14 +1,9 @@
 use diesel::prelude::*;
 use diesel::MysqlConnection;
-use dotenvy::dotenv;
-use std::env;
 
 use crate::errors::MyError;
 
 pub fn establish_connection() -> Result<MysqlConnection, MyError> {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    MysqlConnection::establish(&database_url).map_err(MyError::DatabaseConnectionError)
+    MysqlConnection::establish("mysql://mitch:iHtLfC75&qM2Qj@localhost/casine")
+        .map_err(MyError::DatabaseConnectionError)
 }
