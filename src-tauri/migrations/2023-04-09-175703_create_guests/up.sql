70ec3cbd219f5dc618dafdf7da6ccb49229554e5
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-05-11 17:52:18 +0200

    make tables compliant to sqlite

--- src-tauri/migrations/2023-04-09-175703_create_guests/up.sql
+++ src-tauri/migrations/2023-04-09-175703_create_guests/up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE guests(
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INTEGER NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    birth_date DATE NOT NULL
+    birth_date DATE NOT NULL,
+    PRIMARY KEY (id)
 )
