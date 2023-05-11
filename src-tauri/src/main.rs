b96a16506ac8098c9fb5a97f24268f86dc3c440b
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-05-11 17:53:13 +0200

    add `embed_migrations`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,18 @@ pub mod errors;
 pub mod models;
 pub mod schema;
 
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
 use crate::commands::*;
 
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
+
 fn main() {
+    let mut connection = db::establish_connection().expect("errore connessione db");
+    connection
+        .run_pending_migrations(MIGRATIONS)
+        .expect("Error on migrating");
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             db_commands::get_apartments,
