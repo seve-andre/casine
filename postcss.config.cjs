c2bfd111d28f1c42e287c45fce96339fc74e4090
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-05-18 11:41:40 +0200

    remove `postcss-import` dep

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,7 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const postcssImporter = require("postcss-import");
-
 const config = {
   plugins: [
-    postcssImporter,
-    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    tailwindcss(),
-    //But others, like autoprefixer, need to run after,
-    autoprefixer,
+    require("tailwindcss"),
+    require("autoprefixer")
   ],
 };
 
