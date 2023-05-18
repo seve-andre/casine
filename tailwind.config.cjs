5890ed98249f51a2e1c66c177c5787ae4595aa19
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-05-18 11:43:26 +0200

    remove colors since are not used

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,12 +9,7 @@ const config = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        // flowbite-svelte
-         primary: {50:'#ebf5ff',100: '#fff1ee',200:'#ffe4de',300:'#ffd5cc',400:'#ffbcad',500:'#fe795d',600:'#ef562f',700:'#eb4f27',800:'#d3330a',900:'#d3330a'}
-      }
-    }
+    extend: {}
   }
 };
 
