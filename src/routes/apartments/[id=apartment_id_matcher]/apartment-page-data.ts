910d05adfe45dcf1112dafe6f634f984f4287dd6
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-05-18 10:57:25 +0200

    add `nullable` for optional values

--- src/routes/apartments/[id=apartment_id_matcher]/apartment-page-data.ts
+++ src/routes/apartments/[id=apartment_id_matcher]/apartment-page-data.ts
@@ -1,11 +1,12 @@
 import { z } from "zod"
 import { ApartmentSchema, GroupSchema, GuestSchema, RentSchema } from "~/models"
 
+// json returns "null" if not found, so "nullable()"
 export const ApartmentPageDataSchema = z.object({
     apartment: ApartmentSchema,
-    rent: RentSchema,
-    guests: z.array(GuestSchema),
-    group: GroupSchema
+    rent: RentSchema.nullable(),
+    guests: z.array(GuestSchema).nullable(),
+    group: GroupSchema.nullable()
 })
 
 export type ApartmentPageData = z.infer<typeof ApartmentPageDataSchema>
