a478ec9f3ae7664e09f2fa36f4bf20f4f42b11f6
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-05-19 10:10:54 +0200

    from 4 to 2 spaces

--- src/routes/apartments/[id=apartment_id_matcher]/apartment-page-data.ts
+++ src/routes/apartments/[id=apartment_id_matcher]/apartment-page-data.ts
@@ -3,10 +3,10 @@ import { ApartmentSchema, GroupSchema, GuestSchema, RentSchema } from "~/models"
 
 // json returns "null" if not found, so "nullable()"
 export const ApartmentPageDataSchema = z.object({
-    apartment: ApartmentSchema,
-    rent: RentSchema.nullable(),
-    guests: z.array(GuestSchema).nullable(),
-    group: GroupSchema.nullable()
+  apartment: ApartmentSchema,
+  rent: RentSchema.nullable(),
+  guests: z.array(GuestSchema).nullable(),
+  group: GroupSchema.nullable()
 })
 
 export type ApartmentPageData = z.infer<typeof ApartmentPageDataSchema>
