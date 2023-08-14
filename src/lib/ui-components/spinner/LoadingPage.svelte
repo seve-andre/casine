9bbd6e789efe2fe150989b5c15c096b0ede1ea1b
Andrea Severi <andrea.severi.dev@gmail.com>
2023-08-14 11:23:12 +0200

    add loading spinner and page

---
+++ src/lib/ui-components/spinner/LoadingPage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import LoadingSpinner from "./LoadingSpinner.svelte"
+</script>
+
+<!-- full page and centered -->
+<div class="h-full flex justify-center items-center">
+  <LoadingSpinner size="20" />
+</div>
---
+++ src/lib/ui-components/spinner/LoadingSpinner.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Spinner } from "flowbite-svelte"
+
+  export let size = "16"
+</script>
+
+<Spinner {size} />
