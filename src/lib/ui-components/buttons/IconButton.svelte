f7cc1fa3e9e50d7aa7a8f313c4eaecf76e9dfd99
Andrea Severi <andrea.severi.dev@gmail.com>
2023-08-14 11:22:30 +0200

    change folders name for UI components

---
+++ src/lib/ui-components/buttons/IconButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import OutlinedButton from "./OutlinedButton.svelte"
+
+  export let href: string | undefined = undefined
+  export let type: "button" | "reset" = "button"
+  export let size: "xs" | "sm" | "md" | "lg" | "xl" = "md"
+</script>
+
+<!-- TODO: change bg and fg color on hover -->
+<OutlinedButton on:click {href} {type} {size} class="border-transparent text-black">
+  <slot />
+</OutlinedButton>
--- src/lib/ui-components/index.ts
+++ src/lib/ui-components/index.ts
@@ -1,20 +1,28 @@
-export { default as FilledButton } from "./button/FilledButton.svelte"
-export { default as IconButton } from "./button/IconButton.svelte"
-export { default as OutlinedButton } from "./button/OutlinedButton.svelte"
-export { default as TextButton } from "./button/TextButton.svelte"
+// buttons
+export { default as FilledButton } from "./buttons/FilledButton.svelte"
+export { default as IconButton } from "./buttons/IconButton.svelte"
+export { default as OutlinedButton } from "./buttons/OutlinedButton.svelte"
+export { default as TextButton } from "./buttons/TextButton.svelte"
 
-export { default as LeftArrow } from "./icon/LeftArrow.svelte"
-export { default as RightArrow } from "./icon/RightArrow.svelte"
-export { default as HomeFilled } from "./icon/filled/HomeFilled.svelte"
-export { default as HomeOutlined } from "./icon/outlined/HomeOutlined.svelte"
-export { default as BookFilled } from "./icon/filled/BookFilled.svelte"
-export { default as BookOutlined } from "./icon/outlined/BookOutlined.svelte"
-export { default as PriceTagFilled } from "./icon/filled/PriceTagFilled.svelte"
-export { default as PriceTagOutlined } from "./icon/outlined/PriceTagOutlined.svelte"
+// icons
+export { default as LeftArrow } from "./icons/LeftArrow.svelte"
+export { default as RightArrow } from "./icons/RightArrow.svelte"
+export { default as HomeFilled } from "./icons/filled/HomeFilled.svelte"
+export { default as HomeOutlined } from "./icons/outlined/HomeOutlined.svelte"
+export { default as BookFilled } from "./icons/filled/BookFilled.svelte"
+export { default as BookOutlined } from "./icons/outlined/BookOutlined.svelte"
+export { default as PriceTagFilled } from "./icons/filled/PriceTagFilled.svelte"
+export { default as PriceTagOutlined } from "./icons/outlined/PriceTagOutlined.svelte"
 
-export { default as ErrorNotification } from "./notification/ErrorNotification.svelte"
-export { default as SuccessNotification } from "./notification/SuccessNotification.svelte"
+// notifications
+export { default as ErrorNotification } from "./notifications/ErrorNotification.svelte"
+export { default as SuccessNotification } from "./notifications/SuccessNotification.svelte"
 
+// spinner
+export { default as LoadingSpinner } from "./spinner/LoadingSpinner.svelte"
+export { default as LoadingPage } from "./spinner/LoadingPage.svelte"
+
+// table
 export { default as TableMd } from "./table/TableMd.svelte"
 export { default as TableHeadMd } from "./table/TableHeadMd.svelte"
 export { default as TableBodyCellMd } from "./table/TableBodyCellMd.svelte"
