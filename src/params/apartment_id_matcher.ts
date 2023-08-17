7029e2771f1cdd821f4ff2d33fc4249cd9d301b5
Andrea Severi <andrea.severi.dev@gmail.com>
2023-08-17 10:30:54 +0200

    fix import

--- src/params/apartment_id_matcher.ts
+++ src/params/apartment_id_matcher.ts
@@ -1,6 +1,6 @@
 import type { ParamMatcher } from "@sveltejs/kit"
 
-import { Constants } from "~/lib/utils/constants"
+import { Constants } from "$lib/utils/constants"
 
 export const match = (param => {
   return Constants.apartmentRegex.test(param)
