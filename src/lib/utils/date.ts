71bfe2958a538b4b39d8e3f8e0927353c86cd71a
Andrea Severi <andrea.severi.dev@gmail.com>
2023-08-22 11:02:44 +0200

    rename date utils file

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,7 +1,18 @@
-export function calculate_age(dob: Date): number {
+import { arrayFromRange } from "$lib/utils/array"
+
+export const calculateAge = (dob: Date): number => {
   const diff_ms = Date.now() - dob.getTime()
   const age_dt = new Date(diff_ms)
   return Math.abs(age_dt.getUTCFullYear() - 1970)
+}
+
+export const getMonthNames = (startMonth = 1, endMonth = 12) => {
+  const date = new Date()
+
+  return arrayFromRange(startMonth, endMonth).map(m => {
+    date.setMonth(m - 1)
+    return date.toLocaleString("it-IT", { month: "long" })
+  })
 }
 
 declare global {
